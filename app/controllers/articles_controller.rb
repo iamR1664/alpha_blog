b16fc64d9caa618bfc334f56acaa3485286e99a7
Roba Bawazir <robao1664@gmail.com>
2025-01-11 07:03:11 +0300

    edit and update method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,34 @@ class ArticlesController < ApplicationController
     @articles =Article.all
   end
 
+  
+  def new 
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create 
+    @article = Article.new(params.require(:article).permit(:title,:description))
+    if @article.save
+      flash[:notic] = "atricle was created successfully."
+      redirect_to @article
+    else
+    puts @article.errors.full_messages.join(", ")
+    render "new"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title,:description))
+      flash[:notic] = "atricle was updated successfully."
+      redirect_to @article
+    else
+    puts @article.errors.full_messages.join(", ")
+    render "edit"
+  end
+end
 end
