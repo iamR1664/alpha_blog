12e00dd02a4ae43c1fd15a0e98a239277b6905ff
Roba Bawazir <robao1664@gmail.com>
2025-02-03 20:21:41 +0300

    validtion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-
-end  
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 25}
+  VALID_EMAIL_REJEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true , uniqueness: { case_sensitive: false }, length: {maximum: 105},
+  format: {with: VALID_EMAIL_REJEX }
+end   
