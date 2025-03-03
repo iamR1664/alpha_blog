6836240eebacdc17b4acd27b26edcabfde2092ea
Roba Bawazir <robao1664@gmail.com>
2025-03-04 02:13:19 +0300

    add helper

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
 
   def create 
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = helpers.current_user
     if @article.save
       flash[:notic] = "atricle was created successfully."
       redirect_to articles_path
