672420eaa080961fc9c58130df959dbbd48b05cc
Roba Bawazir <robao1664@gmail.com>
2025-03-13 00:06:37 +0300

    adding categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_05_174219) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_09_214403) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "robas", force: :cascade do |t|
